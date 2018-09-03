34c44f3a16ef178f71e3ada5c7688736410c8cb5
M.Ishimoto <ishitra777@gmail.com>
2018-09-03 09:17:53 +0900

    Initial Commit

---
+++ iRWPickFlavor/iRWPickFlavor.h
@@ -0,0 +1,20 @@
+//
+//  iRWPickFlavor.h
+//  iRWPickFlavor
+//
+//  Created by eagle two on 2018/09/03.
+//  Copyright © 2018年 isshie. All rights reserved.
+//
+        
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iRWPickFlavor.
+FOUNDATION_EXPORT double iRWPickFlavorVersionNumber;
+
+//! Project version string for iRWPickFlavor.
+FOUNDATION_EXPORT const unsigned char iRWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iRWPickFlavor/PublicHeader.h>
+
+
